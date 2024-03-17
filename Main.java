694eda703e5e3034bb0991a09ce45b83f1ff769c
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-17 12:35:56 +0530

    D class Added

--- Main.java
+++ Main.java
@@ -3,15 +3,16 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("manjiri");
         System.out.println("chandure");
-        A a = new A();
-        a.a();
-        a.aa();
-        B b = new B();
-        b.b();
-        C c = new C();
-        c.c();
-        D d = new D();
-        d.d();
+         A a = new A();
+         a.a();
+         a.aa();
+         B b = new B();
+         b.b();
+         C c = new C();
+         c.c();
+         D d = new D();
+         d.d();
+
     }
 
 }
