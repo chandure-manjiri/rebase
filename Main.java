82aa1950476ed25a43481046119a5052b01bc9bd
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-17 12:24:12 +0530

    A class Added

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main {
 
     public static void main(String args[]){
          System.out.println("manjiri");
+
         System.out.println("chandure");
+         A a = new A();
+         a.a();
     }
+
 
 }
